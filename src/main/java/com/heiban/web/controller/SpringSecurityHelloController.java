20500ff4450014e1fdd8c11bc45f903a97132b13
yellowbee <checkudpsum@gmail.com>
2014-09-16 20:32:53 -0500

    home page, course page and template tags created;

--- src/main/java/com/heiban/web/controller/SpringSecurityHelloController.java
+++ src/main/java/com/heiban/web/controller/SpringSecurityHelloController.java
@@ -1,22 +1,53 @@
 package com.heiban.web.controller;
 
-import java.security.Principal;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.heiban.domain.User;
+import com.heiban.service.HeibanService;
 
 @Controller
 public class SpringSecurityHelloController {
+	private HeibanService heibanService;
+	
+	
+	public HeibanService getHeibanService() {
+		return heibanService;
+	}
+
+	@Autowired
+	@Qualifier("heibanService")
+	public void setHeibanService(HeibanService heibanService) {
+		this.heibanService = heibanService;
+	}
+
 	@RequestMapping(value = "/mypage", method = RequestMethod.GET)
-    public String mypage(Model model, Principal principal) {
-        String userName = principal.getName();
-        model.addAttribute("message", "Hi " + userName + ", Welcome to 'Spring Security Custom Login Form Example'");
- 
-        return "mypage";
+    public ModelAndView mypage() {
+		ModelAndView mav = new ModelAndView("page");
+		SecurityContext context = SecurityContextHolder.getContext();
+		Authentication authentication = context.getAuthentication();
+		String username = authentication.getName();
+		User user = heibanService.getUserByUsername(username);
+		mav.addObject("username", username);
+		mav.addObject("firstname", user.getFirstName());
+		mav.addObject("lastname", user.getLastName());
+		mav.addObject("courses", user.getCourses());
+        return mav;
     }
-     
+	
+	@RequestMapping(value="/mycourse", method=RequestMethod.GET)
+	public ModelAndView mycourse() {
+		ModelAndView mav = new ModelAndView("course");
+		return mav;
+	}
+
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public String logoutPage() {
         return "logoutPage";
